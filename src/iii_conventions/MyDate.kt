21adba1d117955609e9962a751716b86ddc0e9f0
Eugeny Karpov <michmanharitonov1@gmail.com>
2017-08-30 09:20:04 +0300

    add 25, 26, 27, 28

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,20 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+import java.util.*
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparable<MyDate> {
+    override fun compareTo(other: MyDate): Int {
+        return this.transformToCalendar().compareTo(other.transformToCalendar())
+    }
+
+    private fun transformToCalendar(): Calendar {
+        val thisDate = Calendar.getInstance()
+        thisDate.set(year, month, dayOfMonth)
+        return thisDate
+    }
+}
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +22,29 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateRange(val start: MyDate, val endInclusive: MyDate):Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> {
+        return object : Iterator<MyDate> {
+            var current = start
+
+            override fun hasNext() = current <= endInclusive
+
+            override fun next(): MyDate {
+                val currentStart = current
+                current = current.nextDay()
+                return currentStart
+            }
+
+        }
+    }
+
+    operator fun contains(date: MyDate) = this.start <= date && this.endInclusive >= date
+}
+
+//operator fun MyDate.compareTo(other: MyDate) = this.transformToCalendar().compareTo(other.transformToCalendar())
+
+//private fun MyDate.transformToCalendar(): Calendar {
+//    val thisDate = Calendar.getInstance()
+//    thisDate.set(year, month, dayOfMonth)
+//    return thisDate
+//}
