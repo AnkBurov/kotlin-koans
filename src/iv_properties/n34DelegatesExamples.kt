477f98ab9ab61d9bcd42dd260fe3ba3d84f91bcd
Eugeny Karpov <michmanharitonov1@gmail.com>
2017-09-01 00:37:12 +0300

    add 34, 35

--- src/iv_properties/n34DelegatesExamples.kt
+++ src/iv_properties/n34DelegatesExamples.kt
@@ -4,8 +4,20 @@ import util.TODO
 import util.doc34
 
 class LazyPropertyUsingDelegates(val initializer: () -> Int) {
-    val lazyValue: Int by todoTask34()
+    val lazyValue: Int by lazy(initializer)
 }
+
+/*class MyDelegate(val initializer: () -> Int) {
+
+    private var value: Int? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): Int {
+        if (value == null) {
+            value = initializer.invoke()
+        }
+        return value!!
+    }
+}*/
 
 fun todoTask34(): Lazy<Int> = TODO(
     """
